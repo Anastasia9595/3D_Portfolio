9cf81245efc351ed04536cd2aedb472f976a63a6
anastasia <Anastasia9595@googlemail.com>
2023-04-16 15:17:14 +0200

    add new Modifications to the model objec

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,28 +4,39 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 //import { CanvasLoader } from "../Loader";
 
-const Box = () => {
+function Model({ url, scale, position, rotation }) {
+  const gltf = useGLTF(url);
   return (
     <mesh>
-      <boxBufferGeometry attach="geometry" />
-      <meshLambertMaterial attach="material" color="white" />
+      <pointLight intensity={0.8} />
+      <primitive
+        object={gltf.scene}
+        scale={scale}
+        position={position}
+        rotation={rotation}
+      />
     </mesh>
   );
-};
-function Model({ url }) {
-  const gltf = useGLTF(url);
-  return <primitive object={gltf.scene} />;
 }
-
-const Computers = () => {};
 
 const ComputersCanvas = () => {
   return (
-    <Suspense>
-      <Canvas>
-        <OrbitControls />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Canvas
+        camera={{ position: [-20, 5, 10], fov: 20 }}
+        gl={{ preserveDrawingBuffer: true }}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 3}
+          minPolarAngle={Math.PI / 3}
+        />
         <ambientLight />
-        <Model url={"./laptop_and_mouse/scene.gltf"} />
+        <Model
+          url={"./laptop_and_mouse/scene.gltf"}
+          scale={[0.05, 0.05, 0.05]}
+          position={[0, -1.8, 0]}
+          rotation={[-0.02, -0.2, -0.1]}
+        />
       </Canvas>
     </Suspense>
   );
